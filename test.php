3cd19dfa055c47b2b59b22834ea3d28b7a5ecd61
Myron Turner <turnermm02@shaw.ca>
2021-11-02 07:46:56 -0500

    Adds form to script

--- test.php
+++ test.php
@@ -2,6 +2,20 @@
 
 if(!defined('DOKU_INC')) define('DOKU_INC', realpath(dirname(__FILE__) . '/../../../') . '/');
 chdir(DOKU_INC . 'bin');
-$res=shell_exec('php ./plugin.php  metadisplay  -n playground');
+
+?>
+ <form action="test.php" method="post">
+<input type="text" name="namespace" placeholder="namespace" />
+<input type="text" name="user" placeholder="user" />
+<input type="submit" name="submit" />
+</form> 
+<?php
+if ( isset( $_POST['submit'] ) ) { 
+echo '<h3>Form POST Method</h3>'; 
+echo 'Your name is ' . $_POST['user'] .'</br> ';
+echo  'Namespace: ' . $_POST['namespace']; 
+ $res=shell_exec('php ./plugin.php  metadisplay  -n ' . $_POST['namespace']);
 echo $res;
 echo "done";
+}
+?>
