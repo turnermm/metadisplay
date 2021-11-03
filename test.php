7c873b52e820c3626ea8a8eeeab5ac87d7795e71
Myron Turner <turnermm02@shaw.ca>
2021-11-03 13:46:04 -0500

    updates to test.php

--- test.php
+++ test.php
@@ -5,16 +5,20 @@ chdir(DOKU_INC . 'bin');
 
 ?>
  <form action="test.php" method="post">
+ <div>
 <input type="text" name="namespace" placeholder="namespace" />
+<input type="text" name="page" placeholder=":page:name" />
 <input type="text" name="user" placeholder="user" />
-<input type="submit" name="submit" /> <input type="submit" value="help" name="help" />
+</div>
+<div style="line-height:2">
+<input type="submit" name="submit"/>
+<input type="submit" value="help" name="help" />
+</div>
 </form> 
 <?php
 if ( isset( $_POST['submit'] ) ) { 
 echo '<h3>Form POST Method</h3>'; 
-   // echo 'Your name is ' . $_POST['user'] .'</br> ';
-   // echo  'Namespace: ' . $_POST['namespace']; 
-    $res=shell_exec('php ./plugin.php  metadisplay  -n ' . $_POST['namespace'] . ' -u  ' . $_POST['user'] );
+    $res=shell_exec('php ./plugin.php  metadisplay  -n ' . $_POST['namespace'] . ' -p ' . $_POST['namespace']  . ' -u  ' . $_POST['user'] );
 echo $res;
 }
 else if ( isset( $_POST['help'] ) ) { 
